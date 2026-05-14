a8e2324792f43ac2b3f1043542b51a35b77fca47
Chesnok <aulmaxxx@gmail.com>
2026-05-14 23:55:53 +0300

    Update helloworld_pi.py

--- helloworld_pi.py
+++ helloworld_pi.py
@@ -1,3 +1,10 @@
+import random
+r = random.randint(1, 5)
+
 def helloworld(text: str) -> str:
-    print(text)
-    return text
+    if r == 1:
+        print("Hello world!")
+        return "Hello World!"
+    else:
+        print(text)
+        return text
